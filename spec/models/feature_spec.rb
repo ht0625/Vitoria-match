8b12b49547d2adffb18f4174070bc6fc5f24c085
hiroki tamura <hiirookii0625@gmail.com>
2020-10-17 00:12:16 +0900

    Featureモデルテストの実装

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Feature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "特徴ラベルがあれば登録できること" do
+  feature = Feature.new(label: '特徴１')
+    expect(feature).to be_valid
+  end
+  it "特徴ラベルがなければ登録できないこと" do
+  feature = Feature.new(label: nil)
+    expect(feature).not_to be_valid
+  end
+  it "特徴ラベルが10文字であれば登録できること" do
+  feature = Feature.new(label: 'A' * 10)
+    expect(feature).to be_valid
+  end
+  it "特徴ラベルが11文字以上であれば登録できないこと" do
+  feature = Feature.new(label: 'A' * 11)
+    expect(feature).not_to be_valid
+  end
 end
