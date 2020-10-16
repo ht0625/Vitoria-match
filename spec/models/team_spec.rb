dcca326c15d5aef5cf044de90bd0e38eed024c23
hiroki tamura <hiirookii0625@gmail.com>
2020-10-17 00:10:56 +0900

    Teamモデルテストの実装

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "チームの名前があれば登録できること" do
+  team = Team.new(name: 'Aチーム')
+    expect(team).to be_valid
+  end
+  it "チームの名前がなければ登録できないこと" do
+  team = Team.new(name: nil)
+    expect(team).not_to be_valid
+  end
+  it "チームの名前が20文字であれば登録できること" do
+  team = Team.new(name: 'A' * 20)
+    expect(team).to be_valid
+  end
+  it "チームの名前が21文字以上であれば登録できないこと" do
+  team = Team.new(name: 'A' * 21)
+    expect(team).not_to be_valid
+  end
 end
