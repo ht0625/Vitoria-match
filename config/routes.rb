a6f7c594662396c42e6be63b7efe9436254677bd
hiroki tamura <hiirookii0625@gmail.com>
2020-10-18 14:26:08 +0900

    User詳細画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:show]
   resources :teams, only: [:new, :create, :show, :edit, :update, :destroy]
   root 'teams#new'
   if Rails.env.development?
