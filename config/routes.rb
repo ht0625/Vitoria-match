f265899a3f8fc3c0fdb7bf26cd5fc5ce82ab35c7
hiroki tamura <hiirookii0625@gmail.com>
2020-10-18 12:06:06 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :teams, only: [:new, :create, :show, :edit, :update, :destroy]
+  root 'teams#new'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
