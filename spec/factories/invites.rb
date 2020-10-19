047a5cca47a6a2aaa9b3db61fb5a144718b21f24
hiroki tamura <hiirookii0625@gmail.com>
2020-10-19 14:52:22 +0900

    Inviteモデルテストの実装

--- spec/factories/invites.rb
+++ spec/factories/invites.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :invite do
     match_day { "2020-10-19 11:29:40" }
-    place { "MyString" }
-    detail { "MyText" }
-    status { false }
-    user { nil }
-    team { nil }
+    place { "会場１" }
+    detail { "詳細１" }
+    status { true }
   end
 end
