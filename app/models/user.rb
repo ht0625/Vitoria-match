a40cd3b5b0d0b4446a2d13201f4e1cd7818fc4cb
hiroki tamura <hiirookii0625@gmail.com>
2020-10-19 09:23:01 +0900

    Userモデルテストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-    has_many :teams
+  has_many :teams
+
+  validates :name, presence: true, length: { maximum: 20 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前、Email、パスワードに正しい入力があれば登録できること" do
+  user = User.new(name: 'Aチーム', email: 'test1@example.com', password: 'test01', password_confirmation: 'test01')
+    expect(user).to be_valid
+  end
+  it "名前の入力がなければ登録できないこと" do
+  user = User.new(email: 'test1@example.com', password: 'test01', password_confirmation: 'test01')
+    expect(user).not_to be_valid
+  end
+  it "Emailの入力がなければ登録できないこと" do
+  user = User.new(name: 'Aチーム', password: 'test01', password_confirmation: 'test01')
+    expect(user).not_to be_valid
+  end
+  it "パスワードの入力がなければ登録できないこと" do
+  user = User.new(name: 'Aチーム', email: 'test1@example.com')
+    expect(user).not_to be_valid
+  end
 end
