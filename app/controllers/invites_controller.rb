badf6b3e8111697b18a20eaa4c32f8e33bf496be
hiroki tamura <hiirookii0625@gmail.com>
2020-10-19 13:42:47 +0900

    Invite登録機能の実装

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,24 @@
 class InvitesController < ApplicationController
+
   def index
     @invites = Invite.all
+  end
+
+  def new
+    @invite = Invite.new
+  end
+
+  def create
+    @invite = current_user.invites.build(invite_params)
+    if @invite.save
+      redirect_to invites_path,notice: "募集を登録しました！"
+    else
+      render  :new
+    end
+  end
+
+  private
+  def invite_params
+    params.require(:invite).permit(:match_day, :place, :detail, :status, :user_id, :team_id)
   end
 end
