5bf16aeb4e9f518dc1faffc332ddcb30c2770513
hiroki tamura <hiirookii0625@gmail.com>
2020-10-20 17:48:31 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+features = ["ポゼッション", "フィジカル", "スタミナ"]
+features.each do |feature|
+  Feature.create!(
+    label: feature
+  )
+end
+
+5.times do |n|
+  name = Faker::Sports::Football.player
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+  2.times do |n|
+    name = Faker::Sports::Football.team
+    Team.create!(
+      name: name,
+      user_id:  User.last.id
+    )
+    FeatureTeam.create!(
+      team_id:  Team.last.id,
+      feature_id:  Feature.last.id
+    )
+  end
+  place = Faker::Address.city
+  Invite.create!(
+    match_day: '2020-12-20 15:00:00',
+    place:  place,
+    detail:  "1#{n}人参加予定です。詳細は連絡でお願いします。",
+    user_id: User.last.id,
+    team_id: Team.last.id
+  )
+end
