86750e42ba90cf9f8a1dc144a52bdb880ee49bab
hiroki tamura <hiirookii0625@gmail.com>
2020-10-20 22:59:08 +0900

    Conversation一覧画面の修正

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   has_many :feature_teams, dependent: :destroy
   has_many :features, through: :feature_teams
+  has_many :invites, dependent: :destroy
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 20 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :teams, dependent: :destroy
   has_many :invites, dependent: :destroy
+  has_many :messages, dependent: :destroy
   validates :name, presence: true, length: { maximum: 20 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
