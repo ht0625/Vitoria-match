66cadc72a4fd09963c7e928508ca5e96ec9b7edb
hiroki tamura <hiirookii0625@gmail.com>
2020-10-24 10:44:44 +0900

    FeatureTeamモデルテストの実装

--- spec/models/feature_team_spec.rb
+++ spec/models/feature_team_spec.rb
@@ -1,5 +1,11 @@
-# require 'rails_helper'
-#
-# RSpec.describe FeatureTeam, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe FeatureTeam, type: :model do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:team) { FactoryBot.create(:team, user: user) }
+  let!(:feature) { FactoryBot.create(:feature) }
+  it "存在するチームと特徴を紐付けて登録できること" do
+  feature_team = FeatureTeam.new(team: team, feature: feature)
+    expect(feature_team).to be_valid
+  end
+end
