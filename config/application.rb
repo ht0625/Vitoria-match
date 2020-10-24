530d7666b9cc9bf1b225fce99cc4fb776ffd72e3
hiroki tamura <hiirookii0625@gmail.com>
2020-10-24 15:21:14 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module VitoriaMatch
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
