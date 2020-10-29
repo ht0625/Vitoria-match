ae20cfc081ad0a9631362357774f8de23b5ed46d
hiroki tamura <hiirookii0625@gmail.com>
2020-10-29 15:57:46 +0900

    User詳細画面の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @invites = @user.invites.all.order(match_day: "ASC")
+    @invites = @user.invites.all.order(match_day: "ASC").page(params[:page]).per(3)
     redirect_to invites_path, notice: "アクセス権限がありません！" if @user.id != current_user.id
   end
 end
